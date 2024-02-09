c9bc3b87fa95a8d72916bb1db5c70901e4a87222
Lilith207 <freekbertens26@gmail.com>
2024-02-09 10:16:01 +0100

    modified:   Check.py

--- Check.py
+++ Check.py
@@ -3,8 +3,14 @@ import numpy.matrixlib as npM
 import numpy as np
 import math
 
-var = [[0, math.pi],[0,0]]
+Mat1 = [[0, 1j],[-1j,0]]
+Mat2 = [[1,0],[0,1j]]
+Mat3 = [[1/math.sqrt(2)],[1/math.sqrt(2)]]
+Mat4 = [[0],[2j]]
 
-print(npM.asmatrix(var))
+print(npM.asmatrix(Mat1))
+print(npM.asmatrix(Mat2))
+print(npM.asmatrix(Mat3))
+print(npM.asmatrix(Mat4))
 
 print("test")
